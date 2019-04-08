c45c603066b6cd1d6eb1b4122796aa8dd518e59a
Clervens Volcy <clervens.volcy@inovva.com>
2019-04-08 10:50:25 -0400

    Add Darknet layer with forward method

--- darknet.py
+++ darknet.py
@@ -6,6 +6,70 @@ import torch.nn as nn
 import torch.nn.functional as F 
 from torch.autograd import Variable
 
+from utils import * 
+
+class Darknet(nn.Module):
+    def __init__(self, cfgfile):
+        super(Darknet, self).__init__()
+        self.blocks = parse_cfg(cfgfile)
+        self.net_info, self.module_list = create_modules(self.blocks)
+    
+    def forward(self, x, CUDA):
+        modules = self.blocks[1:] # remove the first block since it is for the inputs
+        outputs = {}
+
+        write = 0
+        for i, module in enumerate(modules):        
+            module_type = (module["type"])
+            
+            if module_type == "convolutional" or module_type == "upsample":
+                x = self.module_list[i](x)
+    
+            elif module_type == "route":
+                layers = module["layers"]
+                layers = [int(a) for a in layers]
+    
+                if (layers[0]) > 0:
+                    layers[0] = layers[0] - i
+    
+                if len(layers) == 1:
+                    x = outputs[i + (layers[0])]
+    
+                else:
+                    if (layers[1]) > 0:
+                        layers[1] = layers[1] - i
+    
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+                    x = torch.cat((map1, map2), 1)
+                
+    
+            elif  module_type == "shortcut":
+                from_ = int(module["from"])
+                x = outputs[i-1] + outputs[i+from_]
+    
+            elif module_type == 'yolo':        
+                anchors = self.module_list[i][0].anchors
+                #Get the input dimensions
+                inp_dim = int (self.net_info["height"])
+        
+                #Get the number of classes
+                num_classes = int (module["classes"])
+        
+                #Transform 
+                x = x.data
+                x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
+                if not write:              #if no collector has been intialised. 
+                    detections = x
+                    write = 1
+        
+                else:       
+                    detections = torch.cat((detections, x), 1)
+        
+            outputs[i] = x
+        
+        return detections
+
 class DetectionLayer(nn.Module):
     def __init__(self, anchors):
         super(DetectionLayer, self).__init__()
@@ -99,7 +163,8 @@ def create_modules(blocks):
 
         #If it is a route layer
         elif (x["type"] == "route"):
-            x["layers"] = x["layers"].split(',')
+            if isinstance(x["layers"], str):
+                x["layers"] = x["layers"].split(',')
             #Start  of a route
             start = int(x["layers"][0])
             #end, if there exists one.
@@ -145,5 +210,5 @@ def create_modules(blocks):
 
 
 if __name__ == "__main__":
-    blocks = parse_cfg("./cfg/yolov3-tiny-face.cfg")
-    print(create_modules(blocks))
+    net = Darknet("./cfg/yolov3-tiny-face.cfg")
+    print(create_modules(net.blocks))
