58cdb0238438517c116bdaf4dc1ce8f9a4280598
Clervens Volcy <clervens.volcy@inovva.com>
2019-04-08 13:46:49 -0400

    remove empty comment

--- utils.py
+++ utils.py
@@ -129,7 +129,6 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
         
         if image_pred_.shape[0] == 0:
             continue       
-#        
   
         #Get the various classes detected in the image
         img_classes = unique(image_pred_[:,-1])  # -1 index holds the class index
